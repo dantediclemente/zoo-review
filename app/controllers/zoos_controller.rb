de57221d9a0dccc5b778690ce1bc4cb798906f8a
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-06 17:10:10 -0400

    added index, show, and new form page for Zoos

--- app/controllers/zoos_controller.rb
+++ app/controllers/zoos_controller.rb
@@ -10,5 +10,4 @@ class ZoosController < ApplicationController
   def new
     @zoo = Zoos.new
   end
-
 end
--- spec/features/user_views_all_zoos_spec.rb
+++ spec/features/user_views_all_zoos_spec.rb
@@ -1,28 +1,25 @@
 require 'rails_helper'
-
 feature "visitor sees a list of zoos" do
+  let!(:zoo_1) { create :zoos }
+  let!(:zoo_2) { create :zoos }
 
   scenario "sees a list of zoos and link for new zoo" do
-    bronx_zoo = Zoos.create(name: 'Bronx Zoo', address: '33 Boring Ave', city: 'New York', state: 'NY', zip: '02211')
-    franklin_park = Zoos.create(name: 'Franklin Park', address: '43 Somewhere else', city: 'Boston', state: 'MA', zip: '89023')
 
     visit zoos_path
-    expect(page).to have_link "Bronx Zoo"
-    expect(page).to have_link "Franklin Park"
+    expect(page).to have_link(zoo_1.name)
+    expect(page).to have_link(zoo_2.name)
     click_link "Add New Zoo"
     expect(page).to have_content "New Zoo Form"
   end
 
   scenario "clicks link and is taken to show page for given zoo" do
-    bronx_zoo = Zoos.create(name: 'Bronx Zoo', address: '33 Boring Ave', city: 'New York', state: 'NY', zip: '02211')
 
     visit root_path
-    click_link "Bronx Zoo"
-
-    expect(page).to have_content bronx_zoo.name
-    expect(page).to have_content bronx_zoo.address
-    expect(page).to have_content bronx_zoo.city
-    expect(page).to have_content bronx_zoo.state
-    expect(page).to have_content bronx_zoo.zip
+    click_link(zoo_1.name)
+    expect(page).to have_content zoo_1.name
+    expect(page).to have_content zoo_1.address
+    expect(page).to have_content zoo_1.city
+    expect(page).to have_content zoo_1.state
+    expect(page).to have_content zoo_1.zip
   end
 end
