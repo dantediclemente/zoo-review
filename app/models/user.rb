eb1c9a9b1124b2fb6c3e12a56f560cb5c7c30383
Dante DiClemente <diclementedante@gmail.com>
2016-07-06 13:28:34 -0400

    added devise and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true, presence: true 
+  validates :encrypted_password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  it { is_expected.to have_valid(:email).when("testemail@gmail.com") }
+  it { is_expected.to have_valid(:encrypted_password).when("password") }
+  it { is_expected.to_not have_valid(:email).when("", nil) }
+ end
