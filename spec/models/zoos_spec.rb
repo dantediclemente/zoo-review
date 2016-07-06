860f18d2e3544d2ef94e88472500b8b8331cf654
Dante DiClemente <diclementedante@gmail.com>
2016-07-06 12:51:34 -0400

    added zoo table

---
+++ spec/models/zoos_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Zoos, type: :model do
+  it { is_expected.to have_valid(:name).when("Big Zoo") }
+  it { is_expected.to have_valid(:address).when("23 Somesher Road") }
+  it { is_expected.to have_valid(:city ).when("Boston") }
+  it { is_expected.to have_valid(:state).when("MA") }
+  it { is_expected.to have_valid(:zip).when("01234") }
+  it { is_expected.to_not have_valid(:name).when(nil, "") }
+  it { is_expected.to_not have_valid(:address).when(nil, "") }
+  it { is_expected.to_not have_valid(:city).when(nil, "") }
+  it { is_expected.to_not have_valid(:state).when(nil, "") }
+  it { is_expected.to_not have_valid(:zip).when(nil, "", "0987654", "letters") }
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -58,7 +59,13 @@ end
 require "capybara/rails"
 require "valid_attribute"
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
-
