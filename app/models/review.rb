7826c94cd2e2efc3c90a1bcb9fd5487bddc4567e
Yvga Zavorotko <eugenyaz@gmail.com>
2016-07-06 14:12:25 -0400

    fix-merge-confict-n-add-review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,4 @@
 class Review < ActiveRecord::Base
-
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: 1..5, message: 'Rating must be between 1 - 5' }
