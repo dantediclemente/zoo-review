d7fdb333d2e68807c4e70a7fb8350c94d7f591ad
Dante DiClemente <diclementedante@gmail.com>
2016-07-06 13:34:48 -0400

    fixed end

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,4 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_valid(:email).when("testemail@gmail.com") }
   it { is_expected.to have_valid(:encrypted_password).when("password") }
   it { is_expected.to_not have_valid(:email).when("", nil) }
- end
+end
