0daad21f053cac5507c329e37a66e1335312652e
Dante DiClemente <diclementedante@gmail.com>
2016-07-06 14:01:11 -0400

    fixed rake problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160706135021) do
+ActiveRecord::Schema.define(version: 20160706143522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,4 +43,5 @@ ActiveRecord::Schema.define(version: 20160706135021) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
