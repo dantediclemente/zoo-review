6075a38c98c43d8e8cae2f693f65322d209b49cf
J Rakic <jrjela@gmail.com>
2016-07-07 09:55:43 -0400

    Added foreign keys to reviews and zoos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :reviews
+  has_many :zoos
+  has_many :zoos, through: :reviews
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, uniqueness: true, presence: true 
+  validates :email, uniqueness: true, presence: true
   validates :encrypted_password, presence: true
 end
