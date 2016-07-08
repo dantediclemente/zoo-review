7807a4fddf43f5b9229551326662e59eb79063a0
Dante DiClemente <diclementedante@gmail.com>
2016-07-08 14:19:51 -0400

    style fixes

--- spec/features/user_deletes_review_spec.rb
+++ spec/features/user_deletes_review_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 feature "user deletes review" do
-  scenario "user adds a review successfully and deletes successfully" do
+  before (:each) do
     create_zoo
     write_review
+  end
+
+  scenario "user adds a review successfully and deletes successfully" do
     click_link "Delete Review"
     expect(page).to have_content "Review deleted successfully"
     expect(page).to have_content "Franklin Park Zoo"
   end
 
   scenario "user that didn't create review can't delete review" do
-    create_zoo
-    write_review
     sign_out
     sign_in_as_dummy
     visit root_path
