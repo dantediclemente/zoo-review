23351573cdbec41ae7864311c0e427850b57dbac
Dante DiClemente <diclementedante@gmail.com>
2016-07-08 13:56:23 -0400

    style fixes

--- spec/features/user_can_edit_review_spec.rb
+++ spec/features/user_can_edit_review_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
-feature "visitor sees a list of reviews for zoo" do
-  scenario "user adds a review for a zoo successfully" do
+feature "visitor edits a review" do
+  scenario "user edits a review for a zoo successfully" do
     create_zoo
-    fill_in 'Comment', with: "This zoo is awesome!"
-    choose(5)
-    click_button "Add Review"
-    expect(page).to have_content "Review created successfully"
-    expect(page).to have_content "Rating: 5"
-    expect(page).to have_content "This zoo is awesome!"
+    write_review
+
     click_link "Edit Review"
     fill_in 'Comment', with: "This zoo stinks!"
     choose(3)
     click_button "Add Review"
+
     expect(page).to have_content "Review updated successfully"
     expect(page).to have_content "Rating: 3"
     expect(page).to have_content "This zoo stinks!"
