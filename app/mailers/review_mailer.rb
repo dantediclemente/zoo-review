06822feac0129b5582004e2f9a1a2ba87d0e7e7e
Dante DiClemente <diclementedante@gmail.com>
2016-07-13 09:23:36 -0400

    added email notifications to heroku

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,7 +2,7 @@ class ReviewMailer < ApplicationMailer
   def new_review(review)
     @review = review
     mail(
-      to: review.zoo.user.email,
+      to: @review.zoo.user.email,
       subject: "New Review for #{review.zoo.name}"
     )
   end
