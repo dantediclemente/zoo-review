4cbf3b640edce22d8180d6cf824ba36a6b1053e1
Ben Darcy <BenDarcy@Ben-Darcys-MacBook-Pro.local>
2016-07-14 15:53:50 -0400

    more styling

--- react/src/components/Zoo.js
+++ react/src/components/Zoo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Zoo = props => {
   return (
     <li className="zoo">
-      <div>
+      <div className="callout secondary">
         <a href={"/zoos/" + props.id} >{props.name}</a>
       </div>
     </li>
